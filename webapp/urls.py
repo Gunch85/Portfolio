2468e7fb1bc07e7278245f1048feb6a3f4df6915
Gunch85 <gunch1985@gmail.com>
2022-10-13 20:18:51 +0300

    ppc

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
 
-from webapp.views import index, about
+from webapp.views import index, about, search
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
+    path('search/', search, name='search_result'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,5 @@
+from django.db.models import Q
 from django.shortcuts import render
-
 from webapp.models import Project
 
 
@@ -11,3 +11,14 @@ def index(request):
 def about(request):
 
     return render(request, 'webapp/about.html')
+
+
+def search(request):
+    text = request.GET.get('search')
+    if text:
+        projects = Project.objects.filter(
+            Q(title__contains=text)|Q(description__contains=text)
+        )
+    else:
+        projects = Project.objects.none()
+    return render(request, 'webapp/search_result.html', {'projects': projects})
